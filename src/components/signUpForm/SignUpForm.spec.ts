ccb915349e83891f5ff80d72cc653129b6e6af81
jiwlee523 <ddaezi9576@ex-em.com>
2022-09-14 19:05:22 +0900

    [fea] toMatchSnapshot api 사용
    :Why/What:
    - toMatchSnapshot api 호출
    - SignUpForm.spec.ts.snap 파일 생성
    
    :How(Optional):

--- src/components/signUpForm/SignUpForm.spec.ts
+++ src/components/signUpForm/SignUpForm.spec.ts
@@ -3,7 +3,11 @@ import { render, screen } from "@testing-library/vue";
 import SignUpForm from "./SignUpForm.vue";
 
 describe("SignUpForm 테스트", () => {
-  render(SignUpForm);
+  const signUpForm = render(SignUpForm);
+  
+  it('matches snapshot', () => {
+    expect(signUpForm.container).toMatchSnapshot();
+  });
 
   it("SignUpForm 렌더링 시 내부에 UserName, Email, Password INPUT 3개가 표시되어야함1", () => {
     expect(screen.getByPlaceholderText("username")).toBeTruthy();
