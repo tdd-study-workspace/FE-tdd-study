1f9e7680c09d4ee3ef37d67cd5527afb57b1ebfd
kilee <kiee@ex-em.com>
2022-09-22 20:32:00 +0900

    9/22 Study 완료

--- src/components/signUpForm/SignUpForm.spec.ts
+++ src/components/signUpForm/SignUpForm.spec.ts
@@ -1,23 +1,93 @@
-import { describe, it, expect } from "vitest";
-import { render, screen } from "@testing-library/vue";
+import { describe, it, expect, afterEach, beforeEach } from "vitest";
+import { cleanup, render, screen } from "@testing-library/vue";
 import SignUpForm from "./SignUpForm.vue";
+import { fireEvent } from "@testing-library/dom";
 
-describe("SignUpForm 테스트", () => {
-  const signUpForm = render(SignUpForm);
-  
-  it('matches snapshot', () => {
-    expect(signUpForm.container).toMatchSnapshot();
+describe("SignUpForm Input 값 없는 상태", () => {
+
+  // it('matches snapshot', () => {
+  //   expect(signUpForm.container).toMatchSnapshot();
+  // });
+  beforeEach(() => {
+    render(SignUpForm);
   });
 
-  it("SignUpForm 렌더링 시 내부에 UserName, Email, Password INPUT 3개가 표시되어야함1", () => {
-    expect(screen.getByPlaceholderText("username")).toBeTruthy();
+  afterEach(() =>
+    cleanup()
+  );
+
+  it("SignUpForm 렌더링 시 내부에 UserName INPUT이 표시되어야함", () => {
+    expect(screen.getByLabelText("username").tagName).toEqual("INPUT");
+    expect(screen.getByLabelText("username").getAttribute('type')).toEqual('text');
   });
 
-  it("SignUpForm 렌더링 시 내부에 UserName, Email, Password INPUT 3개가 표시되어야함2", () => {
-    expect(screen.getByPlaceholderText("email")).toBeTruthy();
+  it("SignUpForm 렌더링 시 내부에 Email INPUT이 표시되어야함", () => {
+    expect(screen.getByLabelText("email")).toBeTruthy();
+    expect(screen.getByLabelText("email").getAttribute('type')).toEqual('email');
   });
 
-  it("SignUpForm 렌더링 시 내부에 UserName, Email, Password INPUT 3개가 표시되어야함3", () => {
-    expect(screen.getByPlaceholderText("password")).toBeTruthy();
+  it("SignUpForm 렌더링 시 내부에 Pass INPUT이 표시되어야함", () => {
+    expect(screen.getByLabelText("password")).toBeTruthy();
+    expect(screen.getByLabelText("password").getAttribute('type')).toEqual('password');
+  });
+
+  it('SignUpForm 의 모든 입력 필드 값이 채워지지 않으면 Sign Up 버튼이 비활성화 된다.', () => {
+    expect(screen.getByText('Sign Up').hasAttribute('disabled')).toBeTruthy();
+  });
+
+  it('SignUpForm 렌더링 시 Sign Up Button이 포함되어있다.', () => {
+    expect(screen.getByText("Sign Up").tagName).toEqual('BUTTON');
+  });
+
+  it('Sign Up Form의 Email 은 Validation이 수행되지 않아야 한다.', () => {
+    const emailInput = screen.getByLabelText('email');
+    expect(emailInput.innerHTML.length === 0).toBeTruthy();
+  });
+});
+
+describe("SignUpForm Input 값 있는 상태", () => {
+  const setup = () => {
+    const signUpForm = render(SignUpForm);
+    const usernameInput = signUpForm.getByLabelText('username');
+    const emailInput = signUpForm.getByLabelText('email');
+    const passwordInput = signUpForm.getByLabelText('password');
+    const signUpButton = signUpForm.getByText('Sign Up');
+
+    return { signUpForm, usernameInput, emailInput, passwordInput, signUpButton };
+  }
+
+  afterEach(() =>
+    cleanup()
+  );
+
+  it('SignUpForm 의 모든 입력 필드 값이 채워지면 Sign Up 버튼이 활성화 된다.', () => {
+    const {usernameInput, emailInput, passwordInput, signUpButton} = setup();
+
+    fireEvent.change(usernameInput, {target: {value: 'test name'}});
+    fireEvent.change(emailInput, {target: {value: 'hello@world.com'}});
+    fireEvent.change(passwordInput, {target: {value: '1234'}});
+    // ✅TODO 다음 시간에 signUpButton의 disabled 속성을 확인하는 테스트 코드를 작성해야합니다.
+    expect(signUpButton).toEqual(false);
+
+  });
+
+
+  it('Sign Up Form의 Email 은 Validation 절차가 진행되어야 한다.', () => {
+    const { emailInput } = setup();
+    const isValidEmail = (email:string) => {
+      if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email))
+        return true;
+      return (false)
+    }
+    let isValidationRun = null;
+    fireEvent.change(emailInput, {target: {value: 'hello@world.com'}});
+    isValidationRun = isValidEmail(emailInput.innerText);
+    expect(isValidationRun !== null).toBeTruthy();
+  });
+
+  it('Sign Up 버튼을 눌리면 signUp 함수가 실행되어 회원가입 api가 호출된다.', () => {
+    const {signUpButton} = setup();
+    fireEvent.click(signUpButton);
+    expect(signUpButton).toHaveBeenCalled();
   });
 });
--- src/components/signUpForm/SignUpForm.vue
+++ src/components/signUpForm/SignUpForm.vue
@@ -1,15 +1,31 @@
 <template>
-  <div>
-    <input id="username" type="text" placeholder="username" />
-    <input id="email" type="text" placeholder="email" />
-    <input id="password" type="text" placeholder="password" />
-  </div>
+  <form @submit.prevent="signUp">
+    <input aria-label="username" type="text" placeholder="username" v-model="form.username" />
+    <input aria-label="email" type="email" placeholder="email" v-model="form.email" />
+    <input aria-label="password" type="password" placeholder="password" v-model="form.password" />
+    <button :disabled="buttonDisabled">Sign Up</button>
+  </form>
 </template>
 
 <script setup lang="ts">
-  
+import { computed, reactive } from 'vue';
+
+ const form = reactive({
+  username: '',
+  email: '',
+  password: '',
+});
+
+const buttonDisabled = computed(() => {
+  return !form.username || !form.email || !form.password;
+});
+
+const signUp = () => {
+  // TODO: SignUp 로직
+}
+
 </script>
 
 <style scoped>
-  
+
 </style>
