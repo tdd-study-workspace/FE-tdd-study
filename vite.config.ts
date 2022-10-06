5d18beb5d18d5223765bc71b7b685d71af133680
Likilee <gnsdlrl@daum.net>
2022-10-07 02:06:16 +0900

    [fea] testing 환경 jsdom 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import { fileURLToPath, URL } from 'node:url'
-
+/// <reference types="vitest" />
 import { defineConfig } from 'vite'
+import { fileURLToPath, URL } from 'node:url'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
@@ -10,5 +10,8 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  test: {
+    environment: 'jsdom'
+  },
 })
