b7d17d0b013df1868d6fe658d73a37a4b7530c72
Likilee <gnsdlrl@daum.net>
2022-10-21 00:45:17 +0900

    chore: 스토리북 initial 인스톨

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,6 +5,7 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:vue/vue3-recommended',
+    'plugin:storybook/recommended',
     'plugin:prettier/recommended',
   ],
   rules: {
