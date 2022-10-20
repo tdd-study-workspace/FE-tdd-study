cfb5f0494f575015186b8f0edcea3aa0ad620a39
Likilee <gnsdlrl@daum.net>
2022-10-21 03:08:58 +0900

    delete: 필요없는 eslint 설정 삭제

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,26 +6,13 @@ module.exports = {
     'eslint:recommended',
     'plugin:vue/vue3-recommended',
     'plugin:storybook/recommended',
+    '@vue/eslint-config-prettier',
+    '@vue/eslint-config-typescript',
     'plugin:prettier/recommended',
   ],
   rules: {
     // override/add rules settings here, such as:
     // 'vue/no-unused-vars': 'error'qw
   },
-  overrides: [
-    {
-      files: [
-        '**/__tests__/*.{j,t}s?(x)',
-        '**/tests/unit/**/*.spec.{j,t}s?(x)',
-        '*.stories.@(ts|tsx|js|jsx|mjs|cjs)',
-      ],
-      rules: {
-        // example of overriding a rule
-        'storybook/hierarchy-separator': 'error',
-        // example of disabling a rule
-        'storybook/default-exports': 'off',
-      },
-    },
-  ],
   ignorePatterns: ['components.d.ts', 'auto-imports.d.ts'],
 };
