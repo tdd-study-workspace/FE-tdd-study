a170c440283f766c56c4b180a39aa2041598ce1f
Likilee <gnsdlrl@daum.net>
2022-10-21 01:35:19 +0900

    build: storybook setting done

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,5 @@
+import { withSource } from './withSource';
+
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
@@ -7,3 +9,5 @@ export const parameters = {
     },
   },
 };
+
+export const decorators = [withSource]; // Docs 코드 에러가 공식적으로 Fix되면 사용하지 않는다.
