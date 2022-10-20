1a7df20b30ac2f74e8db91ecdcf8a6eaf150ee2a
Likilee <gnsdlrl@daum.net>
2022-10-21 03:11:48 +0900

    첫 스토리 작성

---
+++ src/components/Footer/RealFooter.stories.ts
@@ -0,0 +1,17 @@
+import type { Meta, StoryFn } from '@storybook/vue3';
+import RealFooter from './RealFooter.vue';
+
+export default {
+  title: 'Footer',
+  component: RealFooter,
+} as Meta<typeof RealFooter>;
+
+const Template: StoryFn<typeof RealFooter> = args => ({
+  components: { RealFooter },
+  setup() {
+    return { args };
+  },
+  template: '<real-footer />',
+});
+
+export const Default = Template.bind({});
