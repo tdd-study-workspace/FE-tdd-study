3debaa6bba909c779e3a3b855e036f00c18b26b5
Likilee <gnsdlrl@daum.net>
2022-10-21 03:11:40 +0900

    페이지 생성

---
+++ src/pages/HomePage/HomePage.vue
@@ -0,0 +1,7 @@
+<template>
+  <h1>Welcome Page</h1>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss"></style>
---
+++ src/pages/SignInPage/SignInpage.vue
@@ -0,0 +1,7 @@
+<template>
+  <h1>SignIn Page</h1>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss"></style>
